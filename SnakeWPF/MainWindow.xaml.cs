2304937fc881a9c4ac28c0e2975cc4c252205354
Herbstl <72488119+Herbstl@users.noreply.github.com>
2020-12-11 09:12:07 +0100

    Projektdateien hinzufügen.

---
+++ SnakeWPF/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SnakeWPF
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random random = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Felder();
+            
+        }
+        // hallo
+        public void Felder()
+        {
+            
+            Playground[,] felder = new Playground[20, 20];
+            spielFeld.Background = Brushes.Lavender;
+            for (int y = 0; y < felder.GetLength(0); y++)
+            {
+                for (int x = 0; x < felder.GetLength(1); x++)
+                {
+                    if (y == 0 || (x == 0) || y == 19 || x == 19)
+                    {
+                        felder[y, x] = new Playground(y, x, 3);
+                        spielFeld.Children.Add(felder[y, x].rechteck);
+                    }
+                    else if (true)
+                    {
+
+                    }
+                    else
+                    {
+                        
+                        int state = random.Next(1, 100) < 95 ? 1 : 2 ;
+
+                        
+                        
+                        //if (state >= 1 && state < 6)
+                        //{
+                        //    state = 1;
+                        //}
+                        //else
+                        //{
+                        //    state = 2;
+                        //}
+                        felder[y, x] = new Playground(y, x, state);
+                        spielFeld.Children.Add(felder[y, x].rechteck);
+                    }
+                    
+                }
+            }
+        }
+
+
+
+    }
+}
---
+++ SnakeWPF/Playground.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace SnakeWPF
+{
+    class Playground
+    {
+        int PosX { get; set; }
+        int PosY { get; set; }
+        int PosState { get; set; }
+        int Size = 20;
+        public Rectangle rechteck = new Rectangle();
+
+        public Playground(int posY, int posX, int posState)
+        {
+            PosX = posX * Size;
+            PosY = posY * Size;
+            PosState = posState;
+            rechteck.Width = Size;
+            rechteck.Height = Size;
+            Canvas.SetLeft(rechteck, PosX);
+            Canvas.SetTop(rechteck, PosY);
+            Color();
+            
+        }
+        void Color()
+        {
+            
+            switch (PosState)
+            {
+                case 1:
+                    rechteck.Fill = Brushes.Green;
+                    break;
+                case 2: 
+                    rechteck.Fill = Brushes.Gray;
+                    break;
+                default:
+                    rechteck.Fill = Brushes.Black;
+                    break;
+            }
+            
+        }
+    }
+
+}
